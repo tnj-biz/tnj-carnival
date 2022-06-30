b3d7644ab8f42ca0128245acb2206f669f907117
tnj-biz <108485418+tnj-biz@users.noreply.github.com>
2022-07-01 01:55:26 +0530

    Update msho-tampermonkey.js

--- msho-tampermonkey.js
+++ msho-tampermonkey.js
@@ -63,7 +63,8 @@ $(document).ready(function () {
                     console.log("host" + actualHost);
 
                     createSummaryTable($(".filter-container"), skuCountPending, totalPending);
-
+                    createOrderTimeSummary();
+                    
                 } else if (actualHost == 'https://supplier.meesho.com/panel/msm2w/orders#readytoship') {
                     // ReadyToShip calculation logic : sel div with .order-product relative  checkbox-present -> with 4 children -> .order-product-size -> div .product-sku text-ellipsis display-block but not .product-id -> 2nd span's inner
 
@@ -94,6 +95,7 @@ $(document).ready(function () {
                     console.log("host" + actualHost);
 
                     createSummaryTable($(".filter-container"), skuCountReadyToShip, totalReadyToShip);
+                    createOrderTimeSummary();
                 }
                 
             }
@@ -225,6 +227,123 @@ $(document).ready(function () {
         }
     }
 
+    function createOrderTimeSummary() {
+       var orderHeadings = document.getElementsByClassName("order-view-heading order-price-info");
+        console.log("hololol");
+        console.log(orderHeadings[0].getElementsByTagName("TD")[1].innerHTML.split(",")[0]);
+        console.log(orderHeadings[0].getElementsByTagName("TD")[1].innerHTML.split(",")[1]);
 
+        var dateTimeCatToOrderCount = {};
+        var dateToOrderCount = {};
+        for(orderHeading of orderHeadings) {
+            var dateTime = orderHeading.getElementsByTagName("TD")[1].innerHTML.split(",");
+            var date = dateTime[0];
+
+            var time = dateTime[1].trim();
+
+            var key = null;
+            var hr = parseInt(time.split(":")[0]);
+            var ampm = time.split(" ")[1];
+            if(ampm == "AM") {
+               if(hr >= 6) {
+                   key = date + "-(06-12hr)";
+               } else {
+                   key = date + "-(00_06hr)";
+               }
+            } else {
+               if(hr >= 6) {
+                   key = date + "-(18_24hr)";
+               } else {
+                   key = date + "-(12_18hr)";
+               }
+            }
+
+            var presentCount = dateTimeCatToOrderCount[key];
+            if(presentCount == null) {
+                dateTimeCatToOrderCount[key] = 1;
+            } else {
+                dateTimeCatToOrderCount[key] = presentCount + 1;
+            }
+
+            var presentOrderCount = dateToOrderCount[date];
+            if(presentOrderCount == null) {
+                dateToOrderCount[date] = 1;
+            } else {
+                dateToOrderCount[date] = presentOrderCount + 1;
+            }
+
+        }
+        console.log(dateTimeCatToOrderCount);
+        console.log(dateToOrderCount);
+
+        var dateKeyToPerc = {};
+        for (const [key, value] of Object.entries(dateTimeCatToOrderCount)) {
+            var totalDateOrders = dateToOrderCount[key.split("-")[0]];
+            dateKeyToPerc[key] = Math.round((value*100)/totalDateOrders) + '%';
+        }
+
+        console.log(dateKeyToPerc);
+        createOrderByTimeTable($(".filter-container"), dateKeyToPerc, dateTimeCatToOrderCount);
+    }
+
+    function createOrderByTimeTable(element ,dateKeyToPerc, dateTimeCatToOrderCount) {
+        element.append("<h3>When do we get order in the day ?</h3></br>");
+
+
+        // creating table elements
+        var table = document.createElement('table');
+        // creating table body <tbody> element
+        var tableBody = document.createElement('tbody');
+
+        var row = document.createElement('tr');
+        var cellh = document.createElement('th');
+
+        cellh.style.color = "blue";
+        cellh.style.border = "1px solid grey";
+        cellh.style.textalign = "centre"
+        cellh.style.padding = "8px"
+
+        var cellh1 = cellh.cloneNode(true);
+        var cellh2 = cellh.cloneNode(true);
+        var cellh3 = cellh.cloneNode(true);
+
+        cellh1.appendChild(document.createTextNode("Day (Time)"));
+        row.appendChild(cellh1);
+        cellh2.appendChild(document.createTextNode("Order Count"));
+        row.appendChild(cellh2);
+        cellh3.appendChild(document.createTextNode("Perc of day's order"));
+        row.appendChild(cellh3);
+
+        tableBody.appendChild(row)
+
+        for (const [key, value] of Object.entries(dateTimeCatToOrderCount)) {
+            var row = document.createElement('tr');
+            var cell = document.createElement('td');
+
+            cell.style.color = "navy-";
+            cell.style.border = "1px solid grey";
+            cell.style.textalign = "centre"
+            cell.style.padding = "8px"
+
+
+            var cell2 = cell.cloneNode(true);
+            var cell3 = cell.cloneNode(true);
+
+            // adding array item to it's cell
+            cell.appendChild(document.createTextNode(key));
+            row.appendChild(cell);
+            cell2.appendChild(document.createTextNode(value));
+            row.appendChild(cell2);
+            cell3.appendChild(document.createTextNode(dateKeyToPerc[key]));
+            row.appendChild(cell3);
+            tableBody.appendChild(row);
+        }
+
+        table.appendChild(tableBody);
+        table.style.border = 'solid 1px black';
+        table.style.fontfamily = 'arial, sans-serif';
+        sortTable(table, 0);
+        element.append(table);
+    }
 
 });
